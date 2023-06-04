bf72e7f9490eb9d7c436a1a4a037e4fa1f18cb84
Yevgen Fedorenko <yevgen.fedorenko@outlook.com>
2023-06-03 21:04:16 -0400

    Add integration test project

---
+++ test/Cargo.Data.Application.IntegrationTests/Services/DataTransformationServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Cargo.Data.Application.IntegrationTests.Services;
+
+public class DataTransformationServiceTests: IClassFixture<BaseTest>
+{
+    private readonly IHost Host;
+
+    public DataTransformationServiceTests(BaseTest baseTest)
+    {
+        Host = baseTest.TestHost;
+    }
+
+    [Fact()]
+    public async void MergeAndSummarizeTest()
+    {
+        Assert.True(false, "This test needs an implementation");
+    }
+}
